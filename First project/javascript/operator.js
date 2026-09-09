7e5b629c5f7c34096da2c1460ea2bf9a28300130
Sulemanhussainshah-SI <sulemanhussainshah78@gmail.com>
2026-09-09 09:40:39 +0530

    Add files via upload

---
+++ First project/javascript/operator.js
@@ -0,0 +1,14 @@
+//let result=3+4;
+//console.log(result);
+
+let price = 140;
+let quantity =3;
+let totalprice = price * quantity;
+let discount = totalprice * 0.1;
+let totaldiscountprice = totalprice - discount;;
+
+console.log("price per item :", price);
+console.log("Quantity:", quantity);
+console.log("Total price:", totalprice);
+console.log("Discount 10%:",discount);
+console.log("TotalCoast:", totaldiscountprice);
---
+++ First project/javascript/pri.js
@@ -0,0 +1,23 @@
+/*let price1=499;
+let price2=299;
+
+let total=price1+price2;
+console.log(total);
+
+let name=prompt("enter name");
+let lastname=prompt(" enter last name");
+console.log(name);
+console.log(lastname);
+let fullname=name+lastname;
+console.log(fullname);
+let FullName = name + " " + lastname;
+console.log(FullName);
+*/
+
+
+/*let a =38;
+a = "suleman";
+console.log(typeof a);*/
+let str="23";
+let num=number(str);
+console.log(num ,typeof num)
